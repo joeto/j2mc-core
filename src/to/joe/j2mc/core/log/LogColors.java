7d55d8c5f902ee1d5c6e2a5e19c725fb97015967
Ammar Askar <ammar_askar@hotmail.com>
2012-05-26 11:05:36 +0500

    Hacky solution to fix craftbukkit's jline change

--- src/to/joe/j2mc/core/log/LogColors.java
+++ src/to/joe/j2mc/core/log/LogColors.java
@@ -3,32 +3,31 @@ package to.joe.j2mc.core.log;
 import java.util.EnumMap;
 import java.util.Map;
 
-import jline.ANSIBuffer.ANSICodes;
-
 import org.bukkit.ChatColor;
 
 public class LogColors {
+    static final char ESC = 27;
     private static final Map<ChatColor, String> replacements;
     private static final ChatColor[] colors;
     static {
         colors = ChatColor.values();
         replacements = new EnumMap<ChatColor, String>(ChatColor.class);
-        LogColors.replacements.put(ChatColor.BLACK, ANSICodes.attrib(0));
-        LogColors.replacements.put(ChatColor.DARK_BLUE, ANSICodes.attrib(34));
-        LogColors.replacements.put(ChatColor.DARK_GREEN, ANSICodes.attrib(32));
-        LogColors.replacements.put(ChatColor.DARK_AQUA, ANSICodes.attrib(36));
-        LogColors.replacements.put(ChatColor.DARK_RED, ANSICodes.attrib(31));
-        LogColors.replacements.put(ChatColor.DARK_PURPLE, ANSICodes.attrib(35));
-        LogColors.replacements.put(ChatColor.GOLD, ANSICodes.attrib(33));
-        LogColors.replacements.put(ChatColor.GRAY, ANSICodes.attrib(37));
-        LogColors.replacements.put(ChatColor.DARK_GRAY, ANSICodes.attrib(0));
-        LogColors.replacements.put(ChatColor.BLUE, ANSICodes.attrib(34));
-        LogColors.replacements.put(ChatColor.GREEN, ANSICodes.attrib(32));
-        LogColors.replacements.put(ChatColor.AQUA, ANSICodes.attrib(36));
-        LogColors.replacements.put(ChatColor.RED, ANSICodes.attrib(31));
-        LogColors.replacements.put(ChatColor.LIGHT_PURPLE, ANSICodes.attrib(35));
-        LogColors.replacements.put(ChatColor.YELLOW, ANSICodes.attrib(33));
-        LogColors.replacements.put(ChatColor.WHITE, ANSICodes.attrib(37));
+        LogColors.replacements.put(ChatColor.BLACK, attrib(0));
+        LogColors.replacements.put(ChatColor.DARK_BLUE, attrib(34));
+        LogColors.replacements.put(ChatColor.DARK_GREEN, attrib(32));
+        LogColors.replacements.put(ChatColor.DARK_AQUA, attrib(36));
+        LogColors.replacements.put(ChatColor.DARK_RED, attrib(31));
+        LogColors.replacements.put(ChatColor.DARK_PURPLE, attrib(35));
+        LogColors.replacements.put(ChatColor.GOLD, attrib(33));
+        LogColors.replacements.put(ChatColor.GRAY, attrib(37));
+        LogColors.replacements.put(ChatColor.DARK_GRAY, attrib(0));
+        LogColors.replacements.put(ChatColor.BLUE, attrib(34));
+        LogColors.replacements.put(ChatColor.GREEN, attrib(32));
+        LogColors.replacements.put(ChatColor.AQUA, attrib(36));
+        LogColors.replacements.put(ChatColor.RED, attrib(31));
+        LogColors.replacements.put(ChatColor.LIGHT_PURPLE, attrib(35));
+        LogColors.replacements.put(ChatColor.YELLOW, attrib(33));
+        LogColors.replacements.put(ChatColor.WHITE, attrib(37));
     }
 
     public static String process(String result) {
@@ -39,6 +38,10 @@ public class LogColors {
                 result = result.replaceAll(color.toString(), "");
             }
         }
-        return result + ANSICodes.attrib(0);
+        return result + attrib(0);
+    }
+
+    private static String attrib(int attr) {
+        return ESC + "[" + attr + "m";
     }
 }
