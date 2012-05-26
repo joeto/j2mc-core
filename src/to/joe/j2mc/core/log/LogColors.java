4d1d2dd915133592e136fa5efc8560ac8676228b
Ammar Askar <ammar_askar@hotmail.com>
2012-05-26 11:53:46 +0500

    Proper jline solution

--- src/to/joe/j2mc/core/log/LogColors.java
+++ src/to/joe/j2mc/core/log/LogColors.java
@@ -4,30 +4,37 @@ import java.util.EnumMap;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.Ansi.Attribute;
 
 public class LogColors {
-    static final char ESC = 27;
     private static final Map<ChatColor, String> replacements;
     private static final ChatColor[] colors;
     static {
         colors = ChatColor.values();
         replacements = new EnumMap<ChatColor, String>(ChatColor.class);
-        LogColors.replacements.put(ChatColor.BLACK, attrib(0));
-        LogColors.replacements.put(ChatColor.DARK_BLUE, attrib(34));
-        LogColors.replacements.put(ChatColor.DARK_GREEN, attrib(32));
-        LogColors.replacements.put(ChatColor.DARK_AQUA, attrib(36));
-        LogColors.replacements.put(ChatColor.DARK_RED, attrib(31));
-        LogColors.replacements.put(ChatColor.DARK_PURPLE, attrib(35));
-        LogColors.replacements.put(ChatColor.GOLD, attrib(33));
-        LogColors.replacements.put(ChatColor.GRAY, attrib(37));
-        LogColors.replacements.put(ChatColor.DARK_GRAY, attrib(0));
-        LogColors.replacements.put(ChatColor.BLUE, attrib(34));
-        LogColors.replacements.put(ChatColor.GREEN, attrib(32));
-        LogColors.replacements.put(ChatColor.AQUA, attrib(36));
-        LogColors.replacements.put(ChatColor.RED, attrib(31));
-        LogColors.replacements.put(ChatColor.LIGHT_PURPLE, attrib(35));
-        LogColors.replacements.put(ChatColor.YELLOW, attrib(33));
-        LogColors.replacements.put(ChatColor.WHITE, attrib(37));
+        replacements.put(ChatColor.BLACK, Ansi.ansi().fg(Ansi.Color.BLACK).toString());
+        replacements.put(ChatColor.DARK_BLUE, Ansi.ansi().fg(Ansi.Color.BLUE).toString());
+        replacements.put(ChatColor.DARK_GREEN, Ansi.ansi().fg(Ansi.Color.GREEN).toString());
+        replacements.put(ChatColor.DARK_AQUA, Ansi.ansi().fg(Ansi.Color.CYAN).toString());
+        replacements.put(ChatColor.DARK_RED, Ansi.ansi().fg(Ansi.Color.RED).toString());
+        replacements.put(ChatColor.DARK_PURPLE, Ansi.ansi().fg(Ansi.Color.MAGENTA).toString());
+        replacements.put(ChatColor.GOLD, Ansi.ansi().fg(Ansi.Color.YELLOW).bold().toString());
+        replacements.put(ChatColor.GRAY, Ansi.ansi().fg(Ansi.Color.WHITE).toString());
+        replacements.put(ChatColor.DARK_GRAY, Ansi.ansi().fg(Ansi.Color.BLACK).bold().toString());
+        replacements.put(ChatColor.BLUE, Ansi.ansi().fg(Ansi.Color.BLUE).bold().toString());
+        replacements.put(ChatColor.GREEN, Ansi.ansi().fg(Ansi.Color.GREEN).bold().toString());
+        replacements.put(ChatColor.AQUA, Ansi.ansi().fg(Ansi.Color.CYAN).bold().toString());
+        replacements.put(ChatColor.RED, Ansi.ansi().fg(Ansi.Color.RED).bold().toString());
+        replacements.put(ChatColor.LIGHT_PURPLE, Ansi.ansi().fg(Ansi.Color.MAGENTA).bold().toString());
+        replacements.put(ChatColor.YELLOW, Ansi.ansi().fg(Ansi.Color.YELLOW).bold().toString());
+        replacements.put(ChatColor.WHITE, Ansi.ansi().fg(Ansi.Color.WHITE).bold().toString());
+        replacements.put(ChatColor.MAGIC, Ansi.ansi().a(Attribute.BLINK_SLOW).toString());
+        replacements.put(ChatColor.BOLD, Ansi.ansi().a(Attribute.UNDERLINE_DOUBLE).toString());
+        replacements.put(ChatColor.STRIKETHROUGH, Ansi.ansi().a(Attribute.STRIKETHROUGH_ON).toString());
+        replacements.put(ChatColor.UNDERLINE, Ansi.ansi().a(Attribute.UNDERLINE).toString());
+        replacements.put(ChatColor.ITALIC, Ansi.ansi().a(Attribute.ITALIC).toString());
+        replacements.put(ChatColor.RESET, Ansi.ansi().a(Attribute.RESET).fg(Ansi.Color.DEFAULT).toString());
     }
 
     public static String process(String result) {
@@ -39,9 +46,5 @@ public class LogColors {
             }
         }
         return result + attrib(0);
-    }
-
-    private static String attrib(int attr) {
-        return ESC + "[" + attr + "m";
     }
 }
